913a0c66170fe91d1dc90d15b79af52451c16ab9
Scott Flaster <scottflaster@gmail.com>
2022-05-18 17:21:04 -0700

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,19 +1,12 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "scottflaster"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-  }
-
+*/
   required_version = ">= 0.14.0"
 }
